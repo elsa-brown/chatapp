361bc9999074cd93526a264fe6d3ab356173f5bb
Elsa Brown <elsambrown@gmail.com>
2019-06-17 08:09:03 -0400

    server set up

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
 const path = require('path');
 const DIST_DIR = path.join(__dirname, '../dist');
