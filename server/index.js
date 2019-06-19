b195bfb0e3961835840cc7ec9da5c256c2dfe1e0
Elsa Brown <elsambrown@gmail.com>
2019-06-19 19:47:41 -0400

    fixed socket server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-import express, { static } from 'express';
-import socketio from 'socket.io';
-import socket from './socket.js';
-import { join } from 'path';
+const express = require('express');
+const socketio = require('socket.io');
+const socket = require('./socket.js');
+const path = require('path');
 
 const app = express();
 const socketServer = require('http').Server(app);
 const io = socketio(socketServer);
 
 const port = process.env.PORT || 3000;
-const DIST_DIR = join(__dirname, '../dist');
+const DIST_DIR = path.join(__dirname, '../dist');
 const mockResponse = {
     foo: 'bar',
     bar: 'foo'
 };
 
-app.use(static(DIST_DIR));
+app.use(express.static(DIST_DIR));
 
 app.get('/api', (req, res) => {
     res.send(mockResponse);
@@ -24,6 +24,13 @@ app.get('/', (req, res) => {
     res.sendFile(join(DIST_DIR, 'index.html'));
 });
 
-socket(io);
+io.on('connection', socket => {
+    socket.on('message', message => {
+        console.log('got message: ', message);
+
+        // emit message to sockets
+        socket.emit('got message', message);
+    });
+});
 
 socketServer.listen(port, () => console.log('listening on port ' + port));
--- server/socket-server.js
+++
@@ -1,6 +0,0 @@
-// const socketio = require('socket.io');
-
-// module.export = (app) => {
-//     const socketServer = require('http').Server(app);
-//     const io = socketio(socketServer);
-//     const socket = require('./socket.js');
